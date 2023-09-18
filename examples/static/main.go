53cbe918b30c6c1c02d9e635fae4aa47f36fe28c
hehaowen00 <edmundhe01@gmail.com>
2023-09-18 15:39:33 +1000

    Implement static file handler using wildcard

--- examples/static/main.go
+++ examples/static/main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	pathrouter "github.com/hehaowen00/path-router"
 )
 
 func main() {
 	r := pathrouter.NewRouter()
-	r.Handle(http.MethodGet, "/*", http.FileServer(http.Dir("www")))
+
+	r.Get("/*", func(w http.ResponseWriter, r *http.Request, ps *pathrouter.Params) {
+		path := ps.Get(r.URL.Path, "*")
+
+		if strings.Contains(path, "..") {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+
+		if len(path) == 0 {
+			path = "index.html"
+		}
+
+		bytes, err := os.ReadFile("./www/" + path)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+
+		contentType := mime.TypeByExtension(filepath.Ext(path))
+		w.Header().Add("Content-Type", contentType)
+		w.Write(bytes)
+	})
 
 	addr := ":8000"
 	log.Printf("started server at %s\n", addr)
